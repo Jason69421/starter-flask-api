e97f3b005c87777998e0126a3ce3bcc827c41513
Jason69421 <64152159+Jason69421@users.noreply.github.com>
2023-10-14 00:02:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,26 +39,9 @@ options = {
 ydl = yt_dlp.YoutubeDL(options)
 
 
-# Define the function you want to run continuously
-def my_method():
-    global value
 
-    #result = subprocess.check_output(["yt-dlp", "https://www.youtube.com/watch?v=eJmtVr5vptI"], text=True, stderr=subprocess.STDOUT)
-
-    #threading.Timer(1, my_method).start()
 
         
-
-# Create a thread to run the method
-thread = threading.Thread(target=my_method)
-
-# Start the thread when the Flask app starts
-@app.before_first_request
-def start_thread():
-    my_method()
-    #thread.start()
-
-
 def uploader():
     while True:
         video_url = 'https://www.youtube.com/watch?v=kK-iR6g-V1g'
@@ -70,9 +53,18 @@ def uploader():
             name = "/tmp/" + file
             g_a.upload(file=name,folderId="841555b9-9cda-4871-864b-be3c0149112e")
             os.remove(name)
-        print("Uploaded")
-        #time.sleep(1)
-        print("sleep")
+# Create a thread to run the method
+
+
+# Start the thread when the Flask app starts
+@app.before_first_request
+def start_thread():
+    thread = threading.Thread(target=uploader)
+    thread.start()
+
+
+
+
     
 
 
