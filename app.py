e75894323edc1c4b45947d8bdc8393cee1fc049b
Jason69421 <64152159+Jason69421@users.noreply.github.com>
2023-10-13 20:31:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
 }
 
-output_dir = os.path.join(os.getcwd(), "tmp") + "\\%(title)s.%(ext)s"
+output_dir = os.path.join(os.getcwd(), "tmp") + "/%(title)s.%(ext)s"
     
     
 options = {
