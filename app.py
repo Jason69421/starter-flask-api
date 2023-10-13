d30923b9f3e5077e22845893145678d1df5fe3d3
Jason69421 <64152159+Jason69421@users.noreply.github.com>
2023-10-13 19:01:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,14 +10,30 @@ ydl = yt_dlp.YoutubeDL()
 
 value = ""
 
+options = {
+    'format': 'best',  # Choose the best available video quality
+    'outtmpl': 'downloads/%(title)s.%(ext)s',  # Specify the download location and filename format
+}
+
 # Define the function you want to run continuously
 def my_method():
     global value
-    video_url = 'https://www.youtube.com/watch?v=eJmtVr5vptI'
+    video_url = 'https://www.youtube.com/watch?v=kfchvCyHmsc&pp=ygUONSBzZWNvbmQgdmlkZW8%3D'
     with ydl:
-        video_info = ydl.extract_info(video_url, download=False)
+        result = ydl.extract_info(video_url, download=True, extra_info=options)
+
+    if result['status'] == 'finished':
+        video_filename = result['filename']
+        print(f"Video downloaded to: {video_filename}")
+    
+        # Check the size of the downloaded file
+        if os.path.exists(video_filename):
+            file_size = os.path.getsize(video_filename)
+            print(f"File size: {file_size} bytes")
+        else:
+            print("File not found.")
     #result = subprocess.check_output(["yt-dlp", "https://www.youtube.com/watch?v=eJmtVr5vptI"], text=True, stderr=subprocess.STDOUT)
-    value = video_info['title']
+    value = video_filename
     #threading.Timer(1, my_method).start()
 
         
